ffcc9e8e4de1ae252fbffa90b554c5a4802cc233
Anna <annagirl312@gmail.com>
2024-07-29 20:58:01 +0300

    first task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число a:");
+        int a = scanner.nextInt();
+        System.out.println("Введите число b:");
+        int b = scanner.nextInt();
+        if(a>b){
+            System.out.println("a>b");
+            }
+        if(a<b){
+            System.out.println("a<b");
+        }
+            else{
+                System.out.println("a=b");
+            }
+
+        System.out.println("Результат сложения: " + a + " + " + b+ " = "  + (a + b));
+        System.out.println("Результат деления: " + a + " / " + b+ " = "  + (a / b));
+        System.out.println("Результат вычитания: " + a + " - " + b+ " = "  + (a - b));
+        System.out.println("Результат умножения: " + a + " * " + b+ " = "  + (a * b));
     }
-}
+    }
