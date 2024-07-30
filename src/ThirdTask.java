aef37dfb8d59695df89362c2d07a2acc564406b0
Anna <annagirl312@gmail.com>
2024-07-30 16:04:27 +0300

    third task

--- src/ThirdTask.java
+++ src/ThirdTask.java
@@ -3,9 +3,9 @@ public class ThirdTask {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9,10};
         System.out.println("Массив: [1, 2, 3, 4, 5, 6, 7, 8, 9,10]");
         System.out.println("Четные элементы массива");
-        for (int i = 0; i < arr.length; i++) {
-            if(arr[i]%2==0){
-                System.out.println(arr[i]);
+        for (int number:arr) {
+            if(number%2==0){
+                System.out.println(number);
             }
         }
     }
