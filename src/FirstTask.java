7111a5d75702066950cbbe125f5de06a8ae26728
Anna <annagirl312@gmail.com>
2024-07-31 16:39:30 +0300

    update first task

--- src/FirstTask.java
+++ src/FirstTask.java
@@ -2,24 +2,29 @@ import java.util.Scanner;
 
 public class FirstTask {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите число a:");
-        int a = scanner.nextInt();
-        System.out.println("Введите число b:");
-        int b = scanner.nextInt();
-        if(a>b){
-            System.out.println("a>b");
+        try{
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите число a:");
+            int a = scanner.nextInt();
+            System.out.println("Введите число b:");
+            int b = scanner.nextInt();
+            if(a>b){
+                System.out.println("a>b");
             }
-        if(a<b){
-            System.out.println("a<b");
-        }
+            if(a<b){
+                System.out.println("a<b");
+            }
             else{
                 System.out.println("a=b");
             }
 
-        System.out.println("Результат сложения: " + a + " + " + b+ " = "  + (a + b));
-        System.out.println("Результат деления: " + a + " / " + b+ " = "  + (a / b));
-        System.out.println("Результат вычитания: " + a + " - " + b+ " = "  + (a - b));
-        System.out.println("Результат умножения: " + a + " * " + b+ " = "  + (a * b));
+            System.out.println("Результат сложения: " + a + " + " + b+ " = "  + (a + b));
+            System.out.println("Результат вычитания: " + a + " - " + b+ " = "  + (a - b));
+            System.out.println("Результат умножения: " + a + " * " + b+ " = "  + (a * b));
+            System.out.println("Результат деления: " + a + " / " + b+ " = "  + (a / b));
+        } catch (ArithmeticException e) {
+            System.out.println("Нельзя делить 0");
+        }
+
     }
     }
